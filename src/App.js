847d091ccc891d3e5f7b6d3b87c55dcdbd7c83fd
GeoffreyGuez <geoffrey.guez@chekk.me>
2023-03-24 11:56:46 +0100

    Created files for working game, manage turn (X/O), victory and restart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Board from './Board';
 
 function App() {
+  const [gameState, setGameState] = useState({
+    history: [{
+      squares: Array(9).fill(null),
+    }],
+    stepNumber: 0,
+    xIsNext: true,
+  });
+
+  function handleClick(i) {
+    const history = gameState.history.slice(0, gameState.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = gameState.xIsNext ? 'X' : 'O';
+    setGameState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      stepNumber: history.length,
+      xIsNext: !gameState.xIsNext,
+    });
+  }
+
+  function jumpTo(step) {
+    setGameState({
+      ...gameState,
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+
+  const history = gameState.history;
+  const current = history[gameState.stepNumber];
+  const winner = calculateWinner(current.squares);
+
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else if (!current.squares.includes(null)) {
+    status = 'Draw';
+  } else {
+    status = 'Next player: ' + (gameState.xIsNext ? 'X' : 'O');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game">
+      <div className="game-board">
+        <Board
+          squares={current.squares}
+          onClick={(i) => handleClick(i)}
+        />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>
+          {history.map((step, move) => {
+            const desc = move ?
+              'Go to move #' + move :
+              'Go to game start';
+            return (
+              <li key={move}>
+                <button onClick={() => jumpTo(move)}>{desc}</button>
+              </li>
+            );
+          })}
+        </ol>
+      </div>
     </div>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default App;
